f9359c6505bdb83837774f677482559c6984ae6f
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-12-14 20:17:14 +0900

    show chart

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react'
 import db from "../firebase"
 import { collection, onSnapshot, orderBy, query, doc, updateDoc, increment } from "firebase/firestore"; 
 import Button from '@mui/material/Button';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2'
+import "./Vote.css"
+
+ChartJS.register( CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend );
 
 const Vote = ({id}) => {
     const [posts, setPosts] = useState([]);
+    const [labels, setLabels] = useState([]);
+    const [data, setData] = useState([0]*labels.length);
     // const [count, setCount] = useState(0);
 
     useEffect(() => {
@@ -14,25 +21,80 @@ const Vote = ({id}) => {
         // リアルタイムでデータ取得→onSnapshot
         onSnapshot(q, (querySnapshot) => {
             setPosts(querySnapshot.docs.map((doc) => [doc.id, doc.data()]))
+            setLabels(querySnapshot.docs.map((doc) => [doc.data().username]))
+            setData(querySnapshot.docs.map((doc) => [doc.data().predict_count]))
         })
     }, []);
 
+    // 要変更
     const count_up = (async(id) => {
         const now_data = doc(db, "posts", id);
         await updateDoc(now_data, {
             predict_count: increment(1)
           });
-        console.log(now_data)
+        Open()
+        // console.log(now_data)
     })
 
-    const option = posts.map((post)=><option value={post[1].username}>{post[1].username}</option>)
+    const Get_name = () => {
+        const name = document.name;
+        const num = name.selectedIndex;
+        const str = name.options[num].value;
+        return str
+    }
+
+    const option = posts.map((post)=><option className='name' value={post[1].username}>{post[1].username}</option>)
+
+    const chart_options = {
+        indexAxis: 'y',
+        elements: {
+            bar: {
+                borderWidth: 2,
+            },
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'right',
+            },
+            title: {
+                display: true,
+                text: 'みんなの予想は??'
+            }
+        }
+    }
+
+    const Chartdata = {
+        labels,
+        datasets: [
+            {
+                label: '予想',
+                data: data,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    }
+
+    // 要変更
+    const [opens, setOpens] = useState('close')
+        
+    const Open = () => {
+        if(opens === 'close') {
+            setOpens('open')
+        }
+    }
 
     return (
         <div>
             <select className='name_pulldown' name='name'>
                 {option}
             </select>
-            <Button variant='outlined' onClick={() => count_up(id)}>予想する</Button>
+            <Button variant='outlined' onClick={() => count_up(Get_name())}>予想する</Button>
+            <div className="result_chart">
+                <Bar options={chart_options} data={Chartdata} />
+            </div>
+            
         </div>
     )
 }
